a535857109855e11eaa4f58eed9efe895421afee
Bryan Wieschenberg <wieschb1@tcnj.edu>
2025-03-03 23:54:52 -0500

    a

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -24,7 +24,7 @@ const About: React.FC = () => {
     <> 
       <p id="ToTop" ref={topRef} className="invisible text-white">ToTop</p>
       <h1 
-        ref={ref} className={`text-6xl lg:text-8xl font-bold lg:mt-2 pb-4 drop-shadow-[7px_7px_1.5px_rgba(30,30,160,1)] text-center relative bg-gradient-to-r from-[#0030ff] to-[#c4f9ff] bg-clip-text text-transparent ${visible ? 'opacity-100 translate-y-0 transition-all duration-300 ease-out' : 'opacity-0 translate-y-20'}`}>
+        ref={ref} className={`text-6xl lg:text-8xl font-bold lg:mt-2 pb-4 drop-shadow-[7px_7px_1.5px_rgba(30,30,160,1)] text-center relative bg-gradient-to-r from-[#0030ff] to-[#c4f9ff] bg-clip-text text-transparent ${visible ? 'opacity-100 translate-y-0 transition-all duration-300 ease-out' : 'opacity-0 -translate-y-20'}`}>
         About Me
       </h1>
        
