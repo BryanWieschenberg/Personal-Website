ff94ac003e6631687155a8570020682cfcd310a9
Bryan Wieschenberg <wieschb1@tcnj.edu>
2025-03-03 23:52:34 -0500

    new animations??

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'; 
+import React, { useRef, useEffect, useState } from 'react'; 
 import AvailabilityTimeline from "./subpages/Availability";
 import Orgs from "./subpages/Orgs";
 import Classes from "./subpages/Classes";
@@ -6,19 +6,31 @@ import Skills from "./subpages/Skills";
 
 const About: React.FC = () => { 
   const topRef = useRef<HTMLParagraphElement | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(false);
  
+  useEffect(() => {
+       const observer = new IntersectionObserver(([entry]) => {
+         if (entry.isIntersecting) {
+           setTimeout(() => setVisible(true), 0);
+         }
+       });
+   
+       if (ref.current) observer.observe(ref.current);
+       return () => observer.disconnect();
+     }, []);
+      
   return ( 
     <> 
       <p id="ToTop" ref={topRef} className="invisible text-white">ToTop</p>
       <h1 
-        className="opacity-0 text-6xl lg:text-8xl font-bold lg:mt-2 pb-4 drop-shadow-[7px_7px_1.5px_rgba(30,30,160,1)] text-center relative bg-gradient-to-r from-[#0030ff] to-[#c4f9ff] bg-clip-text text-transparent"
-      > 
-        About Me 
+        ref={ref} className={`text-6xl lg:text-8xl font-bold lg:mt-2 pb-4 drop-shadow-[7px_7px_1.5px_rgba(30,30,160,1)] text-center relative bg-gradient-to-r from-[#0030ff] to-[#c4f9ff] bg-clip-text text-transparent ${visible ? 'opacity-100 translate-y-0 transition-all duration-300 ease-out' : 'opacity-0 translate-y-20'}`}>
+        About Me
       </h1>
        
       <div className="flex flex-col lg:flex-row items-center justify-center mt-4 lg:mt-8 container mx-auto px-4 lg:px-40"> 
         {/* Left Section: Text */} 
-        <div className="opacity-0 lg:w-1/2 mb-8 lg:mb-0"> 
+        <div ref={ref} className={`lg:w-1/2 mb-8 lg:mb-0 ${visible ? 'opacity-100 translate-x-0 transition-all duration-300 ease-out' : 'opacity-0 -translate-x-20'}`}>
           <p className="text-xs lg:text-lg text-[#d7e0e0]"> 
             Hi, I'm Bryan Wieschenberg, a passionate Computer Science student at The College of New Jersey, currently in my 6th semester as a Junior! I'm an aspiring Software Engineer driven by a lifelong love for technology, problem-solving, and innovation. 
             <br/><br/> 
@@ -29,7 +41,7 @@ const About: React.FC = () => {
         </div> 
      
         {/* Right Section: Image */} 
-        <div className="opacity-0 lg:w-1/2 flex justify-center"> 
+        <div ref={ref} className={`opacity-0 lg:w-1/2 flex justify-center ${visible ? 'opacity-100 translate-x-0 transition-all duration-300 ease-out' : 'opacity-0 translate-x-20'}`}> 
           <img 
             src="./assets/images/tcnj.png" 
             alt="About" 
